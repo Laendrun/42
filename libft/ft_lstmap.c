54138bd29c767c6a7eb18668eb7b3967afd75387
Simon <simon.aeby@laendrun.ch>
2022-09-27 21:44:12 +0200

    ft_lstmap.c

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:05:02 by saeby             #+#    #+#             */
-/*   Updated: 2022/09/27 20:23:01 by saeby            ###   ########.fr       */
+/*   Updated: 2022/09/27 21:43:05 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 t_list	ft_lstmap(t_list *lst, void (*f)(void *), void (*del)(void *))
 {
-	
+	t_list			*tmp;
+	t_ilst			*new;
+	unsigned int	i;
+
+	tmp = lst;
+	new = ft_lstnew(f(tmp->content));
+	tmp = tmp->next;
+	while (tmp)
+	{
+		new = ft_lstnew(f(tmp->content));
+		new = new->next;
+		tmp = tmp->next;
+	}
+	return (new);
 }
