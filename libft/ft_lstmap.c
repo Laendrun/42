1ae75336d001d4760ea2a449aebaa705e32447cd
Simon <simon.aeby@laendrun.ch>
2022-09-27 20:51:43 +0200

    test new ssh key

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,10 +6,13 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:05:02 by saeby             #+#    #+#             */
-/*   Updated: 2022/09/27 20:06:01 by saeby            ###   ########.fr       */
+/*   Updated: 2022/09/27 20:23:01 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
+t_list	ft_lstmap(t_list *lst, void (*f)(void *), void (*del)(void *))
+{
+	
+}
