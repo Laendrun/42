67988339415533f81204f19fab12a2352a116300
Simon <simon.aeby@laendrun.ch>
2022-09-28 10:03:45 +0200

    normally working (untested)

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,17 +6,12 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:54:58 by saeby             #+#    #+#             */
-/*   Updated: 2022/09/26 22:56:50 by saeby            ###   ########.fr       */
+/*   Updated: 2022/09/28 09:54:53 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// code wrote usingh this source : https://www.geeksforgeeks.org/write-memcpy/
-// check the discussion board, it has the code implemented in the linux kernel
-// for memmove
-// at the very bottom of the page there's also an explanation for the below
-// code
+#include <stdio.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:46:07 by saeby             #+#    #+#             */
-/*   Updated: 2022/09/26 22:46:37 by saeby            ###   ########.fr       */
+/*   Updated: 2022/09/28 09:49:27 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char	*dest;
+	char			*dest;
+	unsigned int	i;
 
 	dest = (char *) malloc(ft_strlen(s1) + 1);
-	ft_strcpy(dest, s1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = 0;
 	return (dest);
 }
