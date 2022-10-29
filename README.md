e53bdae27d4e34b5455f7a0632a3cefbffdf7d73
Simon Aeby <saeby@student.42lausanne.ch>
2022-10-29 16:34:11 +0200

    LIBFT - added some doxgen comments and updated README

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Here's a list of all projects available in this repository with a status (Finish
 
 | Project   | Started            | Finished ?         | Marked          | Comments                    | Francinette | Francinette strict |
 | --------- | ------------------ | ------------------ | --------------- | --------------------------- | ----------- | ------------------ |
-| [Libft](https://github.com/Laendrun/42/tree/main/libft) | :white_check_mark: | :white_check_mark: | 1st try: 75/100 | Bonus should be working too | 125%        | Only lstmap fails  |
+| [Libft](https://github.com/Laendrun/42/tree/main/libft) | :white_check_mark: | :white_check_mark: | 125/100 | | 125/100    | Only lstmap fails |
 | [Ft_printf](https://github.com/Laendrun/42/tree/main/ft_printf) | :white_check_mark: | :x:                | N/A             |                             | N/A         |                    |
--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -6,13 +6,28 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 23:26:34 by saeby             #+#    #+#             */
-/*   Updated: 2022/10/04 23:11:57 by saeby            ###   ########.fr       */
+/*   Updated: 2022/10/29 13:31:38 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file ft_isascii.c
+ * @author saeby (saeby@42lausanne.ch)
+ * @version 1.0
+ * @date 2022-10-29
+ */
 #include "libft.h"
 
-int	ft_isascii(int c)
+/**
+ * @fn int	ft_isascii(int c)
+ * Check if a character (converted to an int value) is alphabetical.
+ * @brief Check if a character is alphabetical.
+ * @param c character to ckeck
+ * @return int 
+ * @retval 1 (true) if the character is alphabetical
+ * @retval 0 (false) if the character is not alphabetical
+ */
+int	ft_isascii(int c) 
 {
 	if (c >= 0 && c <= 127)
 		return (1);
