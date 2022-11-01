ef6d741f67951c1cf52e672a1fc61251bc9bca73
Simon Aeby <saeby@student.42lausanne.ch>
2022-11-01 10:00:57 +0100

    - flag & width & 0 working with u specifier

--- ft_printf2/printers/ft_print_u.c
+++ ft_printf2/printers/ft_print_u.c
@@ -3,17 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_u.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saeby <saeby>                              +#+  +:+       +#+        */
+/*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 17:20:14 by saeby             #+#    #+#             */
-/*   Updated: 2022/10/31 18:31:31 by saeby            ###   ########.fr       */
+/*   Updated: 2022/11/01 09:58:18 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	_int_len(int nbr);
+static int	_prepend_char(t_flags *flags);
+
 int	ft_print_u(unsigned int nbr, t_flags *flags)
 {
-	(void)	flags;
-	return (ft_putunbr(nbr));
+	int	count;
+	int	i;
+	int	nbr_len;
+
+	count = 0;
+	i = 0;
+	nbr_len = _int_len(nbr);
+	if (flags->space || flags->plus)
+		nbr_len++;
+	if (flags->minus && flags->width > 0 && flags->zero == 0)
+	{
+		if (flags->space || flags->plus)
+			count += _prepend_char(flags);
+		count += ft_putunbr(nbr);
+		while (i++ < flags->width - nbr_len)
+			write(1, " ", 1);
+	}
+	else if (flags->minus == 0 && flags->width > 0 && flags->zero == 0)
+	{
+		while (i++ < flags->width - nbr_len)
+			write(1, " ", 1);
+		if (flags->space || flags->plus)
+			count += _prepend_char(flags);
+		count += ft_putunbr(nbr);
+	}
+	else if (flags->minus && flags->width > 0 && flags->zero)
+	{
+		if (flags->space || flags->plus)
+			count += _prepend_char(flags);
+		count += ft_putunbr(nbr);
+		while (i++ < flags->width - nbr_len)
+			write(1, " ", 1);
+	}
+	else if (flags->minus == 0 && flags->width > 0 && flags->zero)
+	{
+		if (nbr >= 0 && (flags->space || flags->plus))
+			count += _prepend_char(flags);
+		while (i++ < flags->width - nbr_len)
+			write(1, "0", 1);
+		count += ft_putunbr(nbr);
+	}
+	else
+	{
+		count += _prepend_char(flags);
+		count += ft_putunbr(nbr);
+	}
+	return (count);
+}
+
+static int	_prepend_char(t_flags *flags)
+{
+	int	count;
+
+	count = 0;
+	if ((flags->plus == 1) && (flags->space == 1))
+		count += ft_putchar('+');
+	else if ((flags->plus == 1) && (flags->space != 1))
+		count += ft_putchar('+');
+	else if ((flags->plus != 1) && (flags->space == 1))
+		count += ft_putchar(' ');
+	return (count);
+}
+
+static int	_int_len(int nbr)
+{
+	int	count;
+
+	count = 0;
+	if (nbr < 0)
+	{
+		count++;
+		nbr = -nbr;
+	}
+	if (nbr == 0)
+		count++;
+	while (nbr != 0)
+	{
+		nbr = nbr / 10;
+		count++;
+	}
+	return (count);
 }
--- ft_printf2/test.c
+++ ft_printf2/test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saeby <saeby>                              +#+  +:+       +#+        */
+/*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 15:48:45 by saeby             #+#    #+#             */
-/*   Updated: 2022/10/31 22:59:26 by saeby            ###   ########.fr       */
+/*   Updated: 2022/11/01 10:00:08 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int main(void)
 {
-	int	i;
+	/*int	i;
 
 	ft_printf("Bliblablo\n");
 	ft_printf("|%-25c|\n", 'K');
@@ -28,18 +28,21 @@ int main(void)
 	ft_printf("|%-25s|\n", "Ceci est un test");
 	ft_printf("|%10s|\n", "Ceci est un test");
 	ft_printf("|%-10s|\n", "Ceci est un test");
-	ft_printf("%u\n", 2147483652);
+	ft_printf("|%020u|\n", 2147483652);
 	ft_printf("%%\n");
-	ft_printf("|% -5d|\n\n", 154);
-	ft_printf("|%+ -5d|\n\n", 154);
-	ft_printf("|%+010d|\n\n", 154);
-	ft_printf("|%+-010d|\n\n", 154);
-	ft_printf("|% d|\n\n", 154);
-	ft_printf("|%+ d|\n\n", 154);
-	ft_printf("|%+d|\n\n", -154);
-	ft_printf("%#X\n", 154);
+	ft_printf("|% -5d|\n", 154);
+	ft_printf("|%+ -5d|\n", 154);
+	ft_printf("|%+010d|\n", 154);
+	ft_printf("|%+-010d|\n", 154);
+	ft_printf("|% d|\n", 154);
+	ft_printf("|%+ d|\n", 154);
+	ft_printf("|%+d|\n", -154);*/
+	ft_printf("|%-20x|\n", 154);
+	printf("|%-20x|\n", 154);
+	ft_printf("|%20x|\n", 154);
+	printf("|%20x|\n", 154);
+	/*ft_printf("%#X\n", 154);
 	ft_printf("%#x\n", 154);
 	ft_printf("%p\n", &i);
-	printf("|%#20p|\n", &i);
-	ft_printf("%#X\n", 0);
+	ft_printf("%#X\n", 0);*/
 }
