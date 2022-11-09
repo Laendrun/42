455ca59a8ddd21285835f9594d6e640f51869afe
Simon Aeby <saeby@student.42lausanne.ch>
2022-11-09 17:56:55 +0100

    modifier gnl

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -6,9 +6,11 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 09:19:36 by saeby             #+#    #+#             */
-/*   Updated: 2022/11/04 17:54:59 by saeby            ###   ########.fr       */
+/*   Updated: 2022/11/09 11:40:07 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// gcc get_next_line.c get_next_line_utils.c test.c
 
 #include "get_next_line.h"
 
--- libft/__test.c
+++ libft/__test.c
@@ -6,27 +6,15 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 10:16:41 by saeby             #+#    #+#             */
-/*   Updated: 2022/11/09 11:08:07 by saeby            ###   ########.fr       */
+/*   Updated: 2022/11/09 15:19:50 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int	main(void)
 {
-	int	i;
-
-	write(1, "Test\n", 5);
-	ft_printf("Testing ft_printf from libft.\n");
-	ft_printf("%%%% : %%\n");
-	ft_printf("%%d : %d\n", 1548);
-	ft_printf("%%u : %u\n", 3147483648);
-	ft_printf("%%c : %c\n", 'K');
-	ft_printf("%%s : %s\n", "Je suis un test");
-	ft_printf("%%x : %x\n", 0xabcd);
-	ft_printf("%%x : %x\n", 1654);
-	ft_printf("%%X : %X\n", 0xabcd);
-	ft_printf("%%X : %X\n", 1654);
-	ft_printf("%%p : %p\n", &i);
+	printf("%d\n", ft_printf("%f", 154));
 	return (0);
 }
