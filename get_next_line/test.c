01d7650362a403716d0169cb3f8aaedb7057b8fc
Simon Aeby <saeby@student.42lausanne.ch>
2022-11-12 16:55:03 +0100

    restarted

--- get_next_line/test.c
+++ get_next_line/test.c
@@ -6,7 +6,7 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 09:19:36 by saeby             #+#    #+#             */
-/*   Updated: 2022/11/09 11:40:07 by saeby            ###   ########.fr       */
+/*   Updated: 2022/11/12 14:59:55 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,13 @@ int	main(void)
 {
 	int	fd;
 	
-	fd = open("test", O_RDONLY);
+	fd = open("test.txt", O_RDONLY);
 	//fd2 = open("test2", O_RDONLY);
-	printf("%s\n", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	printf("++++++++++++++++++++++++++++\n");
-	printf("%s\n", get_next_line(fd));
+	
+	printf("%s", get_next_line(fd));
 	printf("++++++++++++++++++++++++++++\n");
-	printf("%s\n", get_next_line(fd));
-	printf("++++++++++++++++++++++++++++\n");
-	printf("%s\n", get_next_line(fd));
-	printf("++++++++++++++++++++++++++++\n");
+	
 	return (0);
 }
