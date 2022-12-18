1b748540b06bb8fbee3a38e32e5bb8667b7a4f48
Simon Aeby <saeby@student.42lausanne.ch>
2022-12-18 17:59:46 +0100

    :information_source: made little tweaks on strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,36 +6,28 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:32:15 by saeby             #+#    #+#             */
-/*   Updated: 2022/10/25 13:07:38 by saeby            ###   ########.fr       */
+/*   Updated: 2022/12/18 11:33:03 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	fill_str(char *res, char const *s1, char const *s2);
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char			*res;
-
-	res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!res)
-		return (NULL);
-	fill_str(res, s1, s2);
-	return (res);
-}
-
-static void	fill_str(char *res, char const *s1, char const *s2)
-{
-	unsigned int	i;
-	unsigned int	j;
+	char	*res;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
-	while (s1[j])
-		res[i++] = s1[j++];
-	j = 0;
-	while (s2[j])
-		res[i++] = s2[j++];
-	res[i] = '\0';
+	res = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i];
+	res[j] = 0;
+	return (res);
 }
