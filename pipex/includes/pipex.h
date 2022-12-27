ac7d4563c61623eb90295075fd4e5c43195f19c9
Simon Aeby <saeby@student.42lausanne.ch>
2022-12-27 21:00:25 +0100

    :construction: working on pipex

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,12 +6,17 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 16:37:43 by saeby             #+#    #+#             */
-/*   Updated: 2022/12/27 16:38:06 by saeby            ###   ########.fr       */
+/*   Updated: 2022/12/27 20:48:47 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
 # include "libft.h"
 
 #endif
--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,18 +6,73 @@
 /*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 16:37:06 by saeby             #+#    #+#             */
-/*   Updated: 2022/12/27 16:41:43 by saeby            ###   ########.fr       */
+/*   Updated: 2022/12/27 20:59:24 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// https://tldp.org/LDP/lpg/node11.html
+// https://linuxhint.com/c-execve-function-usage/
+
 #include "pipex.h"
 
-int	main(int ac, char *av[])
+int	main(int ac, char *av[], char *envp[])
 {
 	(void) ac;
 	(void) av;
-	if (ac != 5)
-		pip_error("Wrong number of arguments");
-	ft_printf("pipex is alive!\n");
+	(void) envp;
+	char *const args1[] = {"/bin/ls", NULL};
+	char *const args2[] = {"/usr/bin/grep", "Make", NULL};
+
+	int fd[2];
+	if (pipe(fd) == -1)
+		return (1);
+
+	int pid1 = fork();
+	if (pid1 < 0)
+		return (2);
+
+	if (pid1 == 0)
+	{
+		// child process 1 (ls)
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execve("/bin/ls", args1, envp);
+	}
+
+
+	int pid2 = fork();
+	if (pid2 < 0)
+		return (2);
+
+	if (pid2 == 0)
+	{
+		// child process 2 (grep)
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execve("/usr/bin/grep", args2, envp);
+	}
+
+
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	close(fd[0]);
+	close(fd[1]);
 	return (0);
 }
+
+/* this nonsense works
+int	main(int ac, char *av[], char *envp[])
+{
+	(void) ac;
+	(void) av;
+	//(void) envp;
+
+	//char *binaryPath = "/bin/ls";
+	char *const args[] = {"/bin/ls", "-la", NULL};
+	//char *const env[] = {"HOSTNAME=www.linuxhint.com", NULL};
+	execve("/bin/ls", args, envp);
+	return (0);
+}
+*/
