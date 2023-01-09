429b508385c70db9a392deafdefe3289e9806447
Simon <simon.aeby@laendrun.ch>
2023-01-09 23:33:50 +0100

    :construction: cpp01/ex05

---
+++ cpp/cpp01/ex05/Harl.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saeby <saeby>                              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 23:04:01 by saeby             #+#    #+#             */
+/*   Updated: 2023/01/09 23:25:05 by saeby            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+# include <iostream>
+
+class Harl
+{
+	public:
+		Harl(void);
+		~Harl(void);
+		void	complain(std::string level);
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+};
+
+#endif
